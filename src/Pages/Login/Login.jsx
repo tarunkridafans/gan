07728d322a1cea63afa330d34b930e0cf1453d9e
tarunkridafans <tarun@krida.fans>
2023-02-15 19:58:33 +0530

    phase3

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.scss";
+
 function Login() {
-  return <div className="login">Login</div>;
+  const [loginForm, setLoginForm] = useState({ email: "", password: "" });
+
+  const onChangeHandler = (e) => {
+    const latest = {};
+    latest[e.target.name] = e.target.value;
+    setLoginForm((prev) => {
+      return { ...prev, ...latest };
+    });
+  };
+
+  const loginHandler = () => {
+    console.log(loginForm);
+  };
+  return (
+    <div className="login">
+      <div className="form">
+        <div>
+          <span>Email</span>
+          <input
+            name="email"
+            value={loginForm.email}
+            onChange={onChangeHandler}
+            type="text"
+          />
+        </div>
+        <div>
+          <span>Password</span>
+          <input
+            name="password"
+            value={loginForm.password}
+            onChange={onChangeHandler}
+            type="password"
+          />
+        </div>
+        <a>Forgot Password?</a>
+        <button onClick={loginHandler}>Login</button>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
