5562fd9b73ea3caee2d14ac3dd7d5e3137cd9613
tarunkridafans <tarun@krida.fans>
2023-03-29 09:00:38 +0530

    fixes

--- src/Components/Header3/Header3.jsx
+++ src/Components/Header3/Header3.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
 import "./Header3.scss";
 import { useNavigate } from "react-router-dom";
+import { signOut } from "firebase/auth";
+import { auth } from "../../firebase-config";
 
 function Header3({ user }) {
   const navigate = useNavigate();
@@ -9,9 +11,19 @@ function Header3({ user }) {
       navigate("/login");
     }
   }, []);
+
+  const logOutHandler = async () => {
+    try {
+      await signOut(auth);
+      navigate("/");
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <div className="header3">
       <span className="user-name">{`Hiii ${user?.email}`}</span>
+      <button onClick={logOutHandler}>Logout</button>
     </div>
   );
 }
